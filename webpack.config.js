34338b4b2a79651c47f0c5adbdadfe3f1985c57f
Daniel Howard <danjhoward@gmail.com>
2019-11-15 16:03:04 -0800

    Don't hash images

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
         test: /\.(jpg|png|svg|gif)$/,
         loader: 'file-loader',
         options: {
-          name: 'images/[name].[hash].[ext]',
+          name: 'images/[name].[ext]',
         },
       },
     ]
